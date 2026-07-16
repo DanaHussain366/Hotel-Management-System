e9efc405286a5a7568be4f83b70ad5b7da3a4c63
Lulu Al Ansari <lulu@Lulus-MacBook-Air-5.local>
2026-07-17 01:07:56 +0300

    Rename packages for Module 3

--- src/RestaurantMenu_TableManagement_m3_Main/HotelResturantModule.java
+++ src/RestaurantMenu_TableManagement_m3_Main/HotelResturantModule.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package hotelresturantmodule;
+package RestaurantMenu_TableManagement_m3_Main;
 
 /**
  *
