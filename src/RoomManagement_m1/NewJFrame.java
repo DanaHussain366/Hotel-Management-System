87fcc03ba0d6f4fe9353f275e55a6fcb7983d3b6
jj <jj@localhost>
2026-07-20 00:06:57 +0300

    added gui files

--- src/RoomManagement_m1/NewJFrame.java
+++ src/RoomManagement_m1/NewJFrame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package RoomManagement;
+package RoomManagement_m1;
 
 /**
  *
@@ -72,3 +72,5 @@ public class NewJFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
+
+
