43f1c5f205dcc357ac309081a36e1a8f27018517
Danas <Danas@YER-A-WIZARD>
2026-07-21 21:11:14 +0300

    Initial commit - multi module Java project

---
+++ src/main/java/RestaurantOrderManagement_m4/OrderStatus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package RestaurantOrderManagement_m4;
+
+/**
+ *
+ * @author Dana Hussain
+ * ID: 2241100226
+ * section: 01A
+ */
+public enum OrderStatus {
+    PLACED, PREPARING, SERVED, COMPLETED, CANCELLED
+    
+}
